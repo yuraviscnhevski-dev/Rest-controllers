90c6607593f56d9754d04b7069666ded46f1f850
yuraviscnhevski-dev <yura.viscnhevski@gmail.com .>
2026-03-16 22:04:09 +0300

    I'm Groot!

--- 3.1.4-Rest-controllers-main/3.1.4 Rest controllers/src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ 3.1.4-Rest-controllers-main/3.1.4 Rest controllers/src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -8,16 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        // Для страницы входа (логин)
         registry.addViewController("/login").setViewName("login");
-
-        // Для главной страницы (можно перенаправить на логин или на /people)
         registry.addViewController("/").setViewName("index");
-
-        // У тебя уже есть:
         registry.addViewController("/user").setViewName("user");
-
-        // Можно добавить для ошибок
+        registry.addViewController("/admin").setViewName("admin");   // добавлено
         registry.addViewController("/access-denied").setViewName("access-denied");
     }
 }
